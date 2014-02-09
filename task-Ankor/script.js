69806b5b3dfa9b78f6b45a948a709fa873cd101a
feonit <feonitu@yandex.ru>
2014-02-09 17:09:01 +0300

    update task

--- task-Ankor/script.js
+++ task-Ankor/script.js
@@ -1,69 +1,4 @@
 (function($){
-/*    var customers = [
-        {
-            "id": "001",
-            "firstname": "Татьяна",
-            "middlename": "Владимировна",
-            "lastname": "Бамович",
-            "phone": "+79106961476",
-            "email": "bamovich2013@gmail.com",
-            "address": "115175, Россия, Москва, Котельническая набережная, 33к1, кв. 150",
-            "tin": "123456789012",
-            "groups": ["1"]
-        },
-        {
-            "id": "002",
-            "firstname": "Иван",
-            "middlename": "Иванович",
-            "lastname": "Иванов",
-            "phone": "+79261234567",
-            "email": "ivanoff@gmail.com",
-            "address": "111333, Россия, Москва, Тверская, 12, кв. 100",
-            "tin": "674530098745",
-            "groups": ["1", "3"]
-        },
-        {
-            "id": "003",
-            "firstname": "Петр",
-            "middlename": null,
-            "lastname": "Петров",
-            "phone": null,
-            "email": null,
-            "address": null,
-            "tin": null,
-            "groups": null
-        },
-        {
-            "id": "004",
-            "firstname": "Константин",
-            "middlename": "Константинович",
-            "lastname": "Константинопольский",
-            "phone": "8(495)2345676",
-            "email": null,
-            "address": null,
-            "tin": null,
-            "groups": null
-        }
-    ];
-
-    var customersGroups = [
-        {
-            "id": "1",
-            "title": "Друзья"
-        },
-        {
-            "id": "2",
-            "title": "Родственники"
-        },
-        {
-            "id": "3",
-            "title": "Actis Wunderman Actis Wundermant"
-        },
-        {
-            "id": "4",
-            "title": "Желтушенчики"
-        }
-    ]*/
 
     var customers, customersGroups;
 
@@ -81,8 +16,8 @@
     }
 
     function myFunc(res1, res2){
-        customers = res1;
-        customersGroups = res2;
+        customers = res1[0];
+        customersGroups = res2[0];
 
         for (var i = 0, len = customers.length; i < len; i +=1){
             var tr = $('<tr>'),
